0a269f5451e7cba93b0c1e9dec8b0bdeefd937a8
maribelcuales <maribelcuales@gmail.com>
2020-05-11 12:41:32 +0800

    Corrected file path for PostsPage and SearchBar imports in App.js.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-import PostsPage from "../PostsContainer/PostsPage"; 
-import SearchBar from "../SearchBar/SearchBarContainer"; 
+import PostsPage from "./PostsContainer/PostsPage"; 
+import SearchBar from "./SearchBar/SearchBarContainer"; 
 
 
 const App = () => {
